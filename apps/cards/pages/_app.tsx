8736f31a91c7da22d0122b2bab26173eb088d886
Konstantin Sokolov <ksokolov@iponweb.net>
2022-02-09 16:15:37 +0300

    Adds loading handler

--- apps/cards/pages/_app.tsx
+++ apps/cards/pages/_app.tsx
@@ -1,11 +1,36 @@
 import {AppProps} from 'next/dist/shared/lib/router/router';
 import {Provider} from 'react-redux';
 import '../styles/global.css';
+import '../styles/nprogress.css';
 import Layout from '../components/Layout/Layout';
+import NProgress from 'nprogress';
 import {useStore} from '../store/store';
+import {useEffect} from 'react';
+import {useRouter} from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	const store = useStore(pageProps.initialReduxState);
+	const router = useRouter();
+
+	useEffect(() => {
+		const handleStart = (url: string) => {
+			console.log(`Loading: ${url}`);
+			NProgress.start();
+		};
+		const handleStop = () => {
+			NProgress.done();
+		};
+
+		router.events.on('routeChangeStart', handleStart);
+		router.events.on('routeChangeComplete', handleStop);
+		router.events.on('routeChangeError', handleStop);
+
+		return () => {
+			router.events.off('routeChangeStart', handleStart);
+			router.events.off('routeChangeComplete', handleStop);
+			router.events.off('routeChangeError', handleStop);
+		};
+	}, [router]);
 
 	return (
 		<Provider store={store}>
